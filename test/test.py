11401c53878673e8208bc99a841eeffd25f3ef9d
joerdsonsilva <joerdsonsilva@hotmail.com>
2025-08-26 10:48:28 -0300

    Update

--- test/test.py
+++ test/test.py
@@ -3,38 +3,51 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
-
+from cocotb.triggers import ClockCycles, RisingEdge, FallingEdge, Timer
 
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+ # Set the clock period to 100 ns (10 MHz)
+    clock = Clock(dut.clk, 100, units="ns")
     cocotb.start_soon(clock.start())
-
-    # Reset
-    dut._log.info("Reset")
+ 
+    await ClockCycles(dut.clk, 4) # show startup X state in VCD
+    dut.clk.value = 0
+    dut.rst_n.value = 0
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
+    await ClockCycles(dut.clk, 4) # show propagation of quiescent inputs in VCD
+ 
+ # reset
+    dut._log.info("reset")
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 1000)
+    await Timer(200, units="ns")
+
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    dut.ui_in <= 0b00
+    await ClockCycles(dut.clk, 1500)
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut.ui_in <= 0b01
+    await ClockCycles(dut.clk, 3000)
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    dut.ui_in <= 0b10
+    await ClockCycles(dut.clk, 3000)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    dut.ui_in <= 0b11
+    await ClockCycles(dut.clk, 750)
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 250)
+
+    dut.rst_n.value = 1
+
+    dut.ui_in <= 0b11
+    await ClockCycles(dut.clk, 2000)
+
+    dut.ui_in <= 0b00
+    await ClockCycles(dut.clk, 500)
